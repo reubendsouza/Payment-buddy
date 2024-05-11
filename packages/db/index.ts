91e912d5096af31063419b197da05a777058c3d3
reubendsouza <reubendsouza800@gmail.com>
2024-05-11 18:39:22 +0530

    update user email

--- packages/db/index.ts
+++ packages/db/index.ts
@@ -1,1 +1,10 @@
-export * from "@prisma/client";
+import { PrismaClient } from "@prisma/client";
+
+const prismaClientSingleton = () => {
+  return new PrismaClient();
+};
+
+const prisma: ReturnType<typeof prismaClientSingleton> =
+  prismaClientSingleton();
+
+export default prisma;
