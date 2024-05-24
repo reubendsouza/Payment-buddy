fcafb93821bb5bc526ec91fdcc6e9cb288994863
reubendsouza <reubendsouza800@gmail.com>
2024-05-25 02:28:57 +0530

    add p2p transfer action

---
+++ apps/user-app/app/lib/actions/p2ptransfer.ts
@@ -0,0 +1,46 @@
+"use server";
+
+import { getServerSession } from "next-auth";
+import { authOptions } from "../auth";
+import prisma from "@repo/db/client";
+
+export async function p2pTransfer(transferTo: string, amount: number) {
+  try {
+    const session = await getServerSession(authOptions);
+    const transferFrom = session.user.id;
+
+    const [transferToUser, fromUserBalance] = await Promise.all([
+      prisma.user.findFirst({
+        where: {
+          number: transferTo,
+        },
+      }),
+      await prisma.balance.findUnique({
+        where: {
+          userId: Number(transferFrom),
+        },
+      }),
+    ]);
+
+    if (!transferToUser) {
+      throw new Error("Incorrect user");
+    }
+    if ((fromUserBalance?.amount ?? 0) < amount) {
+      throw new Error("Low balance");
+    }
+
+    await Promise.all([
+      prisma.balance.update({
+        where: { userId: Number(transferFrom) },
+        data: { amount: { decrement: amount } },
+      }),
+      prisma.balance.update({
+        where: { userId: transferToUser.id },
+        data: { amount: { increment: amount } },
+      }),
+    ]);
+  } catch (err) {
+    console.log(err);
+    throw err;
+  }
+}
